b6acf9235df6e0acdc4c0b857b0ec2dba29b42d0
Livio <livio.brunner.lb1@gmail.com>
2021-03-21 16:13:38 +0100

    add test to android app

--- android-app/app/src/main/java/com/example/swipsum/ui/dashboard/DashboardViewModel.kt
+++ android-app/app/src/main/java/com/example/swipsum/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "ArnoldC is my favorite programming language"
     }
     val text: LiveData<String> = _text
 }
---
+++ design-tokens/copy-android.sh
@@ -0,0 +1,5 @@
+cp -rf build/android/light/colors.xml ../android-app/app/src/main/res/values/colors.xml
+cp -rf build/android/dark/colors.xml ../android-app/app/src/main/res/values-night/colors.xml
+
+cp -rf build/android/light/font_dimens.xml ../android-app/app/src/main/res/values/font_dimens.xml
+cp -rf build/android/dark/font_dimens.xml ../android-app/app/src/main/res/values-night/font_dimens.xml
